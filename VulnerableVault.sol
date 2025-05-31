158f922ed7f12f3086f02095a8b7850f4d5b450a
YounPearson <162786745+YounPearson@users.noreply.github.com>
2025-05-31 08:24:13 +0300

    Create  VulnerableVault.sol

---
+++ VulnerableVault.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract VulnerableVault {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient funds");
+
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] -= _amount;
+    }
+}
